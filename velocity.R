7636b16b8b8c89698ec685c747a25dcda1717290
Greg Nishihara <gnishihara@gmail.com>
2020-11-10 14:04:04 +0900

    update to rename

--- velocity.R
+++ velocity.R
@@ -5,14 +5,38 @@ library(lubridate) # 時系列データの解析に必要
 
 data = tibble(fnames = dir("Data", pattern = "[Cc][Ss][Vv]", full.names = TRUE))
 
-data = data %>% 
-  filter(str_detect(fnames, pattern = "Light_45_bise"))
+data = data %>% filter(str_detect(fnames, pattern = "Flow_733"))
 
+
+get_cols = cols_only(`YYYY/MM/DD` = col_character(),
+          `hh:mm:ss`  = col_character(),
+          `Velo[cm/s]` = col_double(),
+          `Dir[Deg]` = col_double(),
+          `Vel EW[cm/s]` = col_double(),
+          `Vel NS[cm/s]` = col_double(),
+          `Temp[ﾟC]` = col_double())
 
 data = data %>% 
   mutate(data = map(fnames, read_csv, 
-                    skip = 9,
-                    col_names = c("n", "date", "time", "raw", "ppfd")))
+                    skip = 36,
+                    locale = locale(encoding = "CP932"),
+                    col_types = get_cols))
+
+
+fn = function(x) c("date", 
+                 "hms", 
+                 "velocity", 
+                 "dir",
+                 "ew",
+                 "ns",
+                 "temperature")
+data = data %>% 
+  mutate(data = map(data, function(df) {
+    df %>% rename_with(fn)
+  })) %>% 
+  unnest(data)
+
+data
 
 start_time = ymd_hms("2019-10-03 17:00:00")
 end_time   = ymd_hms("2019-11-06 08:15:00")
