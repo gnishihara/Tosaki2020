838d563ba787311bd41af32d2f29af73a71f820f
Greg Nishihara <gnishihara@gmail.com>
2020-11-10 14:35:25 +0900

    More velo analysis

--- velocity.R
+++ velocity.R
@@ -51,6 +51,84 @@ end_time   = ymd_hms("2019-11-06 08:15:00")
 data = data %>% 
   mutate(start_time, end_time)
 
+data
 
 ggplot(data) + 
   geom_point(aes(x = datetime, y = velocity))
+
+ggplot(data) + 
+  geom_point(aes(x = datetime, y = temperature))
+
+
+# 外れ値の処理
+# 
+data = data %>% 
+  select(-fnames, -date, -hms, -start_time, -end_time) %>% 
+  mutate(ng = (abs(vy) > 200 | abs(vx) > 200))
+
+data =data  %>% 
+  mutate(velocity =ifelse(ng, NA, velocity)) %>%
+  mutate(dir =ifelse(ng, NA, dir)) %>% 
+  mutate(ew =ifelse(ng, NA, ew)) %>% 
+  mutate(ns =ifelse(ng, NA, ns))
+
+ggplot(data) + 
+  geom_point(aes(x = datetime, y = velocity))
+  
+
+
+# 統計量
+
+# 一日あたり
+floor_date(end_time, "day") - minutes(1)
+
+data = data %>% 
+  filter(between(datetime,
+                 ceiling_date(start_time, "day"),
+                 floor_date(end_time, "day") - minutes(1))) %>% 
+  mutate(date = floor_date(datetime, "day"))
+
+
+data %>% 
+  group_by(date) %>% 
+  summarise(velocity = mean(velocity, na.rm = T))
+
+
+data %>% 
+  group_by(date) %>% 
+  summarise(across(c(velocity, temperature,
+                     ew, ns),
+                   ~mean(.x, na.rm = T)))
+
+
+data_summary = data %>% 
+  group_by(date) %>% 
+  summarise(across(c(velocity, temperature,
+                     ew, ns),
+                   list(mean = ~mean(.x, na.rm=T),
+                        max = ~max(.x, na.rm=T),
+                        sd = ~sd(.x, na.rm=T))))
+data_summary %>%   tail()
+
+
+ggplot(data_summary) +
+  geom_col(aes( x = date,
+                y = velocity_mean)) +
+  geom_errorbar(aes(x = date,
+                    ymin =velocity_mean,
+                    ymax = velocity_mean + velocity_sd),
+                width = 0)
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
