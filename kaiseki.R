2f73db6508623a3680b98512341d0ae917b205f5
Greg Nishihara <gnishihara@gmail.com>
2020-11-10 11:37:50 +0900

    たたむ/ひらく

--- kaiseki.R
+++ kaiseki.R
@@ -12,4 +12,42 @@ library(tidyverse)
 library(lubridate) # 時系列データの解析に必要
 
 
-fnames = dir("Data", pattern = "[Cc][Ss][Vv]", full.names = TRUE)
+data = tibble(fnames = dir("Data", pattern = "[Cc][Ss][Vv]", full.names = TRUE))
+
+data = data %>% 
+  filter(str_detect(fnames, pattern = "Light_45_bise"))
+
+
+data = data %>% 
+  mutate(data = map(fnames, read_csv, 
+                    skip = 9,
+                    col_names = c("n", "date", "time", "raw", "ppfd")))
+
+start_time = ymd_hms("2019-10-03 17:00:00")
+end_time   = ymd_hms("2019-11-06 08:15:00")
+
+data = data %>% 
+  mutate(start_time,
+         end_time)
+
+
+# ひらいてからの処理
+data %>% 
+  unnest(data) %>% 
+  mutate(datetime = str_glue("{date} {time}")) %>% 
+  mutate(datetime = parse_date_time(datetime, "dmY T*!"))
+
+# たたんだままの　data の処理　
+data = data %>% 
+  mutate(data = map(data, function(df) {
+    df %>% 
+      mutate(datetime = str_glue("{date} {time}")) %>% 
+      mutate(datetime = parse_date_time(datetime, "dmY T*!"))
+  })) %>% 
+  unnest(data)
+  
+
+
+
+
+data %>% filter(between(datetime, left = start_time, right = end_time))
